463287f1d295fbcbd7bc49533c478594e4034f9f
xujie <384977866@qq.com>
2022-10-30 22:33:55 +0800

    add apitags 在控制器中添加

--- project-ec-web3/src/user/user.controller.ts
+++ project-ec-web3/src/user/user.controller.ts
@@ -1,7 +1,9 @@
 /* eslint-disable prettier/prettier */
 import { Controller, Get, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('user')
+@ApiTags('userApi')
 export class UserController {
   @Get()
   index() {
